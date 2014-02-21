5416e7b1894195115c2b5f0a926764a7f23ad29e
Leon1236 <ivan.ziko.rajnovi@gmail.com>
2014-02-21 17:22:14 +0100

    Fixes 2

--- main/java/com/leon1236/newend/core/handlers/FuelHandler.java
+++ main/java/com/leon1236/newend/core/handlers/FuelHandler.java
@@ -8,76 +8,13 @@ import com.leon1236.newend.blocks.EndBlocks;
 import com.leon1236.newend.items.ModItems;
 
 import cpw.mods.fml.common.IFuelHandler;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class FuelHandler implements IFuelHandler{
-	
-	private static ArrayList<FuelValue> fuelList = new ArrayList<FuelValue>();
 
-		@Override
-		public int getBurnTime(ItemStack fuel) 
-		{
-		return getFuelValue(fuel);
-		}
-
-	// Add Fuel rates
-	public static void setFuelValues()
-	{
-		addFuel(new ItemStack(ModItems.ItemEndCoal), 6400);
-		addFuel(new ItemStack(ModItems.ItemEndCharcoal), 3200);
-		addFuel(new ItemStack(EndBlocks.BlockEndWood), 1600);
-		addFuel(new ItemStack(EndBlocks.BlockEndPlank), 400);
-		addFuel(new ItemStack(ModItems.ItemEndStick), 200);
-		
-	}
-
-	private static void addFuel(ItemStack stack, int value)
-	{
-		fuelList.add(new FuelValue(stack, value));
-	}
-
-	private static int getFuelValue(ItemStack stack)
-	{
-		for (FuelValue fuelValue : fuelList)
-		{
-			ItemStack stackToCompareTo = fuelValue.getStack();
-
-			return fuelValue.getValue();
-		}
+       public int getBurnTime(ItemStack fuel) {
 
 		return 0;
 	}
 
-	public static class FuelValue
-	{
-		private ItemStack stack;
-		private int value;
-
-		public FuelValue(ItemStack stack, int value)
-		{
-			this.setStack(stack);
-			this.setValue(value);
-		}
-
-		public ItemStack getStack()
-		{
-			return stack;
-		}
-
-		public void setStack(ItemStack stack) 
-		{
-			this.stack = stack;
-		}
-
-		public int getValue()
-		{
-			return value;
-		}
-
-		public void setValue(int value) 
-		{
-			this.value = value;
-		}
-	}
-
-	
 }
